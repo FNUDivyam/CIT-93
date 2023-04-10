90eea53f4e7305b9aff975c859854a9ba6c7806d
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-04-09 22:50:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
-const MY_MPG = []
-const MY_TRIP_COST = []
-
-const updateDOM = (input) => {
-    const divEl = document.querySelector('#output')
-    const p = document.createElement('p')
-    p.textContent = input
-    divEl.appendChild(p)
-}
-
-const trackMPGandCost = (miles, gallons, price = 3.79) => {
-    const MPG  = Math.round(miles/gallons)
-    const tripCost = Math.round(gallons * price)
-    updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`)
-    MY_MPG.push(MPG)
-    MY_TRIP_COST.push(tripCost)
-}
-
-const calculateSUM = (arr) => {
-    let sum = 0
-    // for(let i=0; i < arr.length; i++) {
-    //     sum = sum + arr[i]
-    // }
-    // arr.forEach(element => {
-    //     sum += element
-    // })
-    for(value of arr) {
-       sum += value
+const myArr = []
+// console.log(typeof myObj)
+// console.log(typeof myArr)
+// console.log(myArr === myObj)
+const myNumber = 9
+const myObj = {
+    name: 'Divyam',
+    age: 23,
+    likes: 0,
+    hobbies: ['coding', 'running', 'workout'],
+    mobile: {
+        make: 'pixel 7',
+        OS: 'android'
+    },
+    myGreeting: function(person) {
+        return `${this.name} who loves to ${this.hobbies[0]} says What is UP ${person}`
+    },
+    increaseLikes: function() {
+        this.likes += 1
     }
-    return sum
+}
+const divyObj = myObj
+divyObj.increaseLikes()
+console.log(divyObj)
+divyObj.age = 60
+
+
+
+
+const greeting =  myObj.myGreeting('jane')
+console.log(greeting)
+
+console.log(myObj.mobile.make.length)
+
+for(key in myObj) {
+    console.log(`key ${key} and value ${myObj[key]} `)
 }
 
-const calculateAvg = () => {
-    let sumMPG = calculateSUM(MY_MPG)
-    let sumTripCost = calculateSUM(MY_TRIP_COST)
-    let avgMPG = Math.round(sumMPG/MY_MPG.length)
-    let avgTripCost = Math.round(sumTripCost/MY_TRIP_COST.length)
-    updateDOM(`Average MPG is ${avgMPG}`)
-    updateDOM(`Average Trip is ${avgTripCost}`)
-}
 
 
-trackMPGandCost(360, 15, 5.40)
-trackMPGandCost(320, 12, 5)
-trackMPGandCost(100, 7, 4.40)
-trackMPGandCost(600, 24, 5.70)
-trackMPGandCost(50, 2, 3.40)
-trackMPGandCost(320, 12, 5)
-calculateAvg()
+
+
