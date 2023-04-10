129f41adaf4404e34055e0eee0e9f2febe520240
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-04-09 21:55:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-alert('Hello World')
+// declare a variable using let and store of value of type string in it
+let myString = "Hello, world!";
 
-console.log('Hello World')
+// declare a variable using let and store of value of type number in it
+let myNumber = 123;
+
+// declare a variable using let and store of value of type boolean in it
+let myBoolean = true;
+
+// console.log the VALUES stored in the VARIABLES
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+
+// use typeof to console log the TYPE of the VALUES stored in the VARIABLES
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
